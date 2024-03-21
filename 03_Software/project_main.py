fd9130522e86412f1f5414a5e2d4b140abaa1ce5
Sultan Palamut <clammyvictor@gmail.com>
2024-03-22 01:06:51 +0300

    updating the lay out of the repo

--- 03_Software/project_main.py
+++ 03_Software/project_main.py
@@ -11,7 +11,7 @@ GPIO.setmode(GPIO.BCM)  # BCM pin-numbering scheme from Raspberry Pi
 
 # Set related pin numbers
 N_To_A = 9  # BCM pin 9, BOARD pin 21
-A_To_N = 5 # D5
+A_To_N = 6 # D6 pin 31
 
 # Set pin as an output pin with optional initial state of HIGH
 GPIO.setup(N_To_A, GPIO.OUT, initial=GPIO.LOW)
@@ -30,12 +30,12 @@ serial_port = serial.Serial(
 # -------------------------- AI Initiate -------------------------- 
 
 our_model = detectNet(model="/home/grup8/Desktop/ELE495/5_obj_model/ssd-mobilenet.onnx", labels="/home/grup8/Desktop/ELE495/5_obj_model/labels.txt", input_blob="input_0", output_cvg="scores", output_bbox="boxes", threshold=0.5)
-net = detectNet("ssd-mobilenet-v2", threshold=0.5)
+net = detectNet("ssd-mobilenet-v2", threshold=0.4)
 
 camera = videoSource("/dev/video0")  # '/dev/video0' for V4L2
-#display = videoOutput("display://0") # 'my_video.mp4' for file
 
 # Initiate gstreamer early
+# in order to save time
 
 img = camera.Capture()
 
@@ -48,7 +48,8 @@ not_detected = net.Detect(img)
 # Wait a second to let the port initialize
 time.sleep(1)
 
-usleep = lambda x: time.sleep(x/1000000.0)
+# Microsecond sleep, uncomment for use
+# usleep = lambda x: time.sleep(x/1000000.0)
 
 # -------------------------- Global Variables --------------------------
 apple_count = 0
@@ -76,28 +77,24 @@ unknown_change = 0
 # 7 => Move the box out of the way
 
 def detection_parser(input_detection):
-    # First ai
-
+    # First AI
     global apple_count
     global banana_count
     global lemon_count
     global orange_count
     global pen_count
-    global unknown_count
     
     global apple_change
     global banana_change
     global lemon_change
     global orange_change
     global pen_change
-    global unknown_change
     
     apple_change = 0
     banana_change = 0
     lemon_change = 0
     orange_change = 0
     pen_change = 0
-    unknown_change = 0
 
     for detection in input_detection:
         if detection.ClassID == 1:
@@ -116,6 +113,20 @@ def detection_parser(input_detection):
         elif detection.ClassID == 5:
             pen_count += 1
             pen_change += 1
+        
+def unknown_parser(input_unknown):
+
+    global unknown_count
+    global unknown_change
+    unknown_change = 0
+
+    for unknown in input_unknown:
+        if unknown.ClassID == 77:
+            unknown_count += 1
+            unknown_change += 1
+        elif unknown.ClassID == 74:
+            unknown_count += 1
+            unknown_change += 1
     
 def fsm():
 
@@ -141,13 +152,16 @@ def fsm():
             # Wait for Center
             print("Current fsm state: ", fsm_state)
             A_Answer = GPIO.input(A_To_N)
+            
             while(A_Answer == 0):
                 A_Answer = GPIO.input(A_To_N)
+            
+            time.sleep(1.5)
+            
             fsm_state = 3
             
         elif fsm_state == 3:
-            
-            #confirmation = input("Confirm to move forward:")
+
             time.sleep(1)
              
             img = camera.Capture()
@@ -158,8 +172,6 @@ def fsm():
             fsm_state = 4
         
         elif fsm_state == 4:
-        
-            #confirmation = input("Confirm to move forward:")
     
             print("Current fsm state: ", fsm_state)
         
@@ -173,8 +185,6 @@ def fsm():
             fsm_state = 5
         
         elif fsm_state == 5:
-    
-            #confirmation = input("Confirm to move forward:")
         
             print("Current fsm state: ", fsm_state)
             
@@ -188,29 +198,23 @@ def fsm():
             fsm_state = 6
         
         elif fsm_state == 6:
-    
-            #confirmation = input("Confirm to move forward:")
         
             print("Current fsm state: ", fsm_state)
             
-            # Write parser for both ai model outputs:
+            # Call parser for both ai model outputs:
             detection_parser(detections)
+            unknown_parser(not_detected)
+            
             # Combine saved data
-            #bluetooth_message = '|' + str(apple_count) + '|' + str(banana_count) + '|' + str(lemon_count) + '|' + str(orange_count) + '|' + str(pen_count) + '|' + str(unknown_count) + '|' + str(apple_change) + '|' + str(banana_change) + '|' + str(lemon_change) + '|' + str(orange_change) + '|' + str(pen_change) + '|' + str(unknown_change)
-            
-            bluetooth_message = '|' + str(apple_count) + '|' + str(banana_count) + '|' + str(lemon_count) + '|' + str(orange_count) + '|' + str(pen_count) + '|' + str(unknown_count)
-            
-            #for x in range(6):
-            #    time.sleep(0.3)
-                # Transmit via UART
-            #    serial_port.write(bluetooth_message.encode())
-                
+            bluetooth_message = str(apple_count) + '|' + str(banana_count) + '|' + str(lemon_count) + '|' + str(orange_count) + '|' + str(pen_count) + '|' + str(unknown_count) + '|' + str(apple_change) + '|' + str(banana_change) + '|' + str(lemon_change) + '|' + str(orange_change) + '|' + str(pen_change) + '|' + str(unknown_change) 
+                 
             # Transmit via UART
             serial_port.write(bluetooth_message.encode())
     
             fsm_state = 7
 
         elif fsm_state == 7:
+        
             # Move box out of the way
             print("Current fsm state: ", fsm_state)
             GPIO.output(N_To_A,GPIO.HIGH)
@@ -222,6 +226,7 @@ def fsm():
             while(A_Answer == 0):
                 A_Answer = GPIO.input(A_To_N)
             fsm_state = 1
+            time.sleep(1.5)
         
         else:
             state = 0
